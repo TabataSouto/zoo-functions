cdfee95d66f61dea4ceaeb17deb19e7f8be6e3c9
Tabata <tabatac.souto@gmail.com>
2022-01-06 17:41:17 -0300

    Requisito 01: correçao lint

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -18,5 +18,3 @@ function getSpeciesByIds(...ids) {
 }
 
 module.exports = getSpeciesByIds;
-
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'));
