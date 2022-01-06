1dffaba572f9a08c48780332b1e6807be8c75de8
Tabata <tabatac.souto@gmail.com>
2022-01-06 16:22:58 -0300

    Requisito 01: finalizado.

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+// caso o parametro seja vazio retornar [];
+// ao receber um id como parâmetro, retorna o array da espécie;
+// ao receber mais de um id, retorna array das espécies;
+
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  const { species } = data;
+  // como o retorno final é um array, é preciso declarar um array vazio para armazenar o resultado;
+  const findSpecies = [];
+
+  // o forEach serve para percorrer todos os ids declarados no parametro ao chamar a função, e ao fazer isso, é atribuido o resultado dentro do nosso array vazio;
+  ids.forEach((id) => {
+    findSpecies.push(species.find((specie) => id === specie.id));
+  });
+  return findSpecies;
 }
 
 module.exports = getSpeciesByIds;
+
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', 'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'));
