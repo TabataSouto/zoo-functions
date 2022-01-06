e5676e486d46e744306bb23e956a71891073e39e
Tabata <tabatac.souto@gmail.com>
2022-01-06 18:36:24 -0300

    Requisito 03: finalizado.

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
+// Esta função é responsável pela busca das pessoas colaboradoras através do primeiro ou do último nome delas
+// Sem parâmetros, retorna um objeto vazio
+// Quando provido o primeiro nome do funcionário, retorna o objeto do funcionário
+// Quando provido o último nome do funcionário, retorna o objeto do funcionário
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  const { employees } = data;
+  // para trazer objeto vazio caso o parametro não seja preenchido;
+  if (!employeeName) { return {}; }
+  // find para encontrar o nome passado pelo parâmetro e retornar o resultado;
+  const finfEmployee = employees.find((employee) =>
+    employee.firstName === employeeName || employee.lastName === employeeName);
+  return finfEmployee;
 }
 
 module.exports = getEmployeeByName;
+
+console.log(getEmployeeByName('Wishart'));
