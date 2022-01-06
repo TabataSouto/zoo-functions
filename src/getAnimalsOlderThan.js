fe5f021578cce57f50deb1eb323ccbe4b8beeb50
Tabata <tabatac.souto@gmail.com>
2022-01-06 17:39:57 -0300

    Requisito 02: finalizado.

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+// Ao passar o nome de uma espécie e uma idade, testa se todos os animais desta espécie possuem a idade mínima especificada.
+
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const { species } = data;
+  const findSpecies = species
+    .filter((specie) => specie.name === animal)[0]
+    .residents.every((val) => val.age >= age);
+
+  return findSpecies;
 }
 
+console.log(getAnimalsOlderThan('otters', 7));
 module.exports = getAnimalsOlderThan;
