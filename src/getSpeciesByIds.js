e441c4f06bc904ed40caca1f80225f30afc574f1
Tabata <tabatac.souto@gmail.com>
2022-01-10 11:07:52 -0300

    Requisito 01: removendo uma linha desnecessaria

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -6,13 +6,11 @@ const { species } = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   // seu código aqui
-  const findSpecies = ids
+  return ids
     // map para percorrer os ids passados no parâmetro na chamada da função;
     .map((idsParam) => species
       // find para encontrar o id passado no parametro dentro do objeto species;
       .find(({ id }) => id === idsParam));
-
-  return findSpecies;
 }
 
 module.exports = getSpeciesByIds;
